e44360eef945c4dab83c61b56e22704d43ab87c3
shariarhossain23e <shariarhossain23@gamil.comm>
2022-06-09 00:39:29 +0600

    navbar

---
+++ src/component/Navbar/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../../assests/developer.png";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div>
+      <Navbar className="header" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logo} width={"70"} alt="" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mx-auto header-link">
+              <a href="#home">Home</a>
+              <a href="#link">About</a>
+              <a href="#link">Contact</a>
+            
+            </Nav>
+            <div>
+            <button className="hire-btn">Hire Me</button>
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
